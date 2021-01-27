7becf4b3628dfdf8a4765d18fbdf89d618d455a2
Tien Nguyen <ttienism@gmail.com>
2021-01-27 17:17:00 +0200

    responsive works for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def icon_for( name, type: :solid, **html_options )
+    fa_class = []
+    type = '' if type == :none
+    fa_class << "fa#{type.to_s[0]}"
+    fa_class << "fa-#{name.dasherize}"
+    fa_class << "fa-2x"
+    fa_class << html_options[:class] if html_options[:class]
+    tag.i **html_options.merge(class: fa_class.join(' '))
+  end
 end
