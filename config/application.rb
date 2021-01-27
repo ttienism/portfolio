d50b33e815a2c71460d93640303340619987f50f
Tien Nguyen <ttienism@gmail.com>
2021-01-27 23:15:20 +0200

    update to try heroku again

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module TienNgPortfolioSite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,5 +18,6 @@ module TienNgPortfolioSite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.initialize_on_precompile = false
   end
 end
