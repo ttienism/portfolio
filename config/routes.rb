6c9011b74cfcc0cc1c2556d86bf46f6fb4171af5
Tien Nguyen <ttienism@gmail.com>
2021-01-27 14:27:21 +0200

    responsive home page | still need some fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  resource :home, only: [ :index ]
+  
+  resource :contact, only: [ :show ], controller: 'contact'
+  resource :about, only: [ :show ], controller: 'about'
+
 end
